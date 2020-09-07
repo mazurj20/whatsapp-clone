3d71ee50bd31147cf06ce11baae0fa2cf8b8875f
Jacob Mazur <jacobmazur@Jacobs-MacBook-Pro.local>
2020-09-07 13:51:55 -0400

    changes

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:9000",
+  baseURL: "http://https://whatspp-clone-backend.herokuapp.com/",
 });
 
 export default instance;
